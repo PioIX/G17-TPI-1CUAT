b260b57b1ca033bbfda19c9fa109c7a0e052c9f6
Boby <101183953+MatteoBobasso@users.noreply.github.com>
2022-07-06 09:45:42 -0300

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,1 +1,38 @@
-a
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#app.secret_key = ""
+
+@app.route("/")
+def mostar_index():
+  return render_template('index.html')
+
+@app.route("/register")
+def guardar_nickname():
+  conn = sqlite3.connect('ODS.db')
+
+  q = f"""INSERT INTO Nombres
+      Values (1)"""
+  z = conn.execute(q)
+  conn.commit(z)
+  return render_template('nickname.html')
+  conn.close()
+  
+@app.route("/ranking")
+def ranking():
+  conn = sqlite3.connect('ODS.db')
+
+  q = f"""SELECT * from Ranking
+          ORDER BY Puntaje DESC
+          LIMIT 10"""
+  x = conn.execute(q)
+  return render_template('ranking.html', lineamostrar = x)
+  conn.close()
+
+@app.route("/información")
+def informacion():
+  return render_template('informacion.html')
+
+app.run(host='0.0.0.0', port=81)
