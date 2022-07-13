2929c58f14bec15238ec5b2c49788b53fbeb6db3
NicolasGilRodriguez <101183758+NicolasGilRodriguez@users.noreply.github.com>
2022-07-13 10:19:04 -0300

    CAMBIOS

--- Proyecto/static/scripts/main.js
+++ Proyecto/static/scripts/main.js
@@ -4,6 +4,32 @@ for (var i = 0; i < pix.length; i++) {
   pix[i].style.animationDelay = Math.ceil(Math.random()*5000)+"ms";
 }
 
-function jugar(){
-  
+/* function jugar(){
+
 }
+
+class Laptop{
+  constructor(id, puntaje){
+    this.puntaje = puntaje;
+    this.id = id;
+  }
+}
+
+Laptop1 = new Laptop(1, 25);
+Laptop7 = new Laptop(7, 50)
+Laptop13 = new Laptop(13, 100) 
+
+class Jugador{
+  constructor(nickname){
+    this.nickname = nickname;
+    this.puntos = [];
+  }
+
+  ganarPuntos()
+}
+
+
+function mostarPuntos25(){
+  document.getElementById("tablet-25").innerHTML;
+   
+}*/
